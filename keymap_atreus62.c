bd53ac39626ee4cc45f7418b0935058370db259c
sevinty7 <dschmell@liberty.edu>
2017-04-04 11:50:10 +0700

    Add files via upload

---
+++ keymap_atreus62.c
@@ -0,0 +1,16 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  KEYMAP(1,  2,   3,   4,   5,   6,   7,   8,   Z,   0,   A, B, \
+             1,  2,   3,   4,   5,   6,   7,   8,   X,   0,   A, B, \
+             1,  2,   3,   4,   5,   6,   7,   8,   C,   0,   A, B, \
+             1,  2,   3,   4,   5,   6,   7,   8,   V,   0,   A, B, \
+             1,  2,   3,   4,   5,   6,   7,   8,   B,   0,   A, B, \
+             Q, W),
+
+
+};
+const action_t PROGMEM fn_actions[] = {
+    
+
+};
